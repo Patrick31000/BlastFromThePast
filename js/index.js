c3ba6cd369adbf94c63aa4f4203cb3ce88f836d9
PatTaylor <patrick.taylor31@gmail.com>
2017-02-13 23:31:01 +0100

    KO

--- js/index.js
+++ js/index.js
@@ -15,79 +15,59 @@ $(document).ready(function() {
 
 
     $("#play").click(function() {
-        $("#bulle").text("Veuillez entrer un nombre positif minimum");
+        $("#bulle").text("Entre un nombre positif minimum");
         $("#champ").submit(function() {
             min = $("#nom").val();
             console.log(min);
             $("#play").unbind("click");
         });
+    });
+    $("#submit").click(function() {
+        min = $("#nom").val();
+        console.log(min);
+        $("#bulle").text("Entre un nombre positif maximum");
+        $("#champ").unbind("submit");
+        $("#champ").submit(function() {
+            max = $("#nom").val();
+            console.log(max);
+
+
+
+
+
+
+
+
+
+            numRand = Math.floor(Math.random() * (max - min + 1) | 0 + min);
+        });
+
         $("#submit").click(function() {
-            min = $("#nom").val();
-            console.log(min);
-            $("#bulle").text("Veuillez entrer un nombre positif maximum");
+            Tentative = $("#nom").val();
+            console.log(Tentative);
+            $("#bulle").text("Combien d'essais veux-tu tenter ?");
             $("#champ").unbind("submit");
-            $("#champ").submit(function() {
-                max = $("#nom").val();
-                console.log(max);
-
-
-
-
-
-
-
-
-
-                numRand = Math.floor(Math.random() * (max - min + 1) | 0 + min);
-            });
 
             $("#submit").click(function() {
-                Tentative = $("#nom").val();
-                console.log(Tentative);
-                $("#bulle").text("Combien d'essais voulez vous tenter ?");
+                num = $("#nom").val();
+                console.log(num);
+                $("#bulle").text("Essaye de trouver le nombre aléatoire");
                 $("#champ").unbind("submit");
-
-                $("#submit").click(function() {
-                    num = $("#nom").val();
-                    console.log(num);
-                    $("#bulle").text("Essayez de trouver le nombre aléatoire ?");
-                    $("#champ").unbind("submit");
-                    $("#champ").submit(function() {
-                        for (i = 0; i < Tentative - 1; i++) {
-
-                            if (numRand > num) {
-                                var num = prompt("c'est plus");
-                            } else if (numRand < num) {
-                                var num = prompt("c'est moins");
-                            } else if (numRand == num) {
-                                alert("GG!!");
-                                break;
-                            }
-                        }
-
-                    });
-                })
             });
-        });
+            $("#champ").submit(function() {
+                for (i = 0; i < Tentative - 1; i++) {
+
+                    if (numRand > num) {
+                        var num = prompt("c'est plus");
+                    } else if (numRand < num) {
+                        var num = prompt("c'est moins");
+                    } else if (numRand == num) {
+                        alert("GG!!");
+                        break;
+                    }
+                }
+
+            });
+        })
     });
 });
-
-
-
-
-
-
-//     // var Tentative = prompt("Combien d'essais voulez vous tenter ?");
-
-//     // var num = prompt("Essayez de trouver le nombre aléatoire");
-//     for (i = 0; i < Tentative - 1; i++) {
-
-//         if (numRand > num) {
-//             var num = prompt("c'est plus");
-//         } else if (numRand < num) {
-//             var num = prompt("c'est moins");
-//         } else if (numRand == num) {
-//             alert("GG!!");
-//             break;
-//         }
-//     }
