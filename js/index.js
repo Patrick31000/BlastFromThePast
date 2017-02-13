358b01176ba5f4f9f642a0f76f59001fb09ff163
PatTaylor <patrick.taylor31@gmail.com>
2017-02-13 17:02:33 +0100

    Min et max OK

--- js/index.js
+++ js/index.js
@@ -1,21 +1,47 @@
-alert("Le but du jeu est de trouver un nombre aléatoire !!!");
+$(document).ready(function() {
 
-var min = prompt("Veuillez entrer un nombre positif minimum");
-var max = prompt("Veuillez entrer un nombre positif maximum");
+    $("#bulle").text("Arriveras tu à deviner le nombre mystère ???");
 
-var numRand = Math.floor(Math.random() * (max - min + 1) | 0 + min);
+    console.log("test");
 
-var Tentative = prompt("Combien d'essais voulez vous tenter ?");
 
-var num = prompt("Essayez de trouver le nombre aléatoire");
-for (i = 0; i < Tentative - 1; i++) {
+    $("#play").click(function() {
+        $("#bulle").text("Veuillez entrer un nombre positif minimum");
+        $("#champ").submit(function() {
+            var min = $("#nom").val();
+            console.log(min);
+            $("#play").unbind("click");
+        });
+        $("#submit").click(function() {
+            var min = $("#nom").val();
+            console.log(min);
+            $("#bulle").text("Veuillez entrer un nombre positif maximum");
+            $("#champ").unbind("submit");
+            $("#champ").submit(function() {
+                var max = $("#nom").val();
+                console.log(max);
+            })
+        });
+    })
+});
 
-    if (numRand > num) {
-        var num = prompt("c'est plus");
-    } else if (numRand < num) {
-        var num = prompt("c'est moins");
-    } else if (numRand == num) {
-        alert("GG!!");
-        break;
-    }
-}
+
+//     // var min = prompt("Veuillez entrer un nombre positif minimum");
+//     // var max = prompt("Veuillez entrer un nombre positif maximum");
+
+//     var numRand = Math.floor(Math.random() * (max - min + 1) | 0 + min);
+
+//     // var Tentative = prompt("Combien d'essais voulez vous tenter ?");
+
+//     // var num = prompt("Essayez de trouver le nombre aléatoire");
+//     for (i = 0; i < Tentative - 1; i++) {
+
+//         if (numRand > num) {
+//             var num = prompt("c'est plus");
+//         } else if (numRand < num) {
+//             var num = prompt("c'est moins");
+//         } else if (numRand == num) {
+//             alert("GG!!");
+//             break;
+//         }
+//     }
