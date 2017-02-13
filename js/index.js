558c8f98467a77fbdf62ce009a3cfdcf45310cc6
PatTaylor <patrick.taylor31@gmail.com>
2017-02-13 22:41:08 +0100

    Alert KO

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+var min;
+var max;
+var Tentative;
+var num;
+var numRand;
+
+
+
+
 $(document).ready(function() {
 
     $("#bulle").text("Arriveras tu à deviner le nombre mystère ???");
@@ -8,28 +17,65 @@ $(document).ready(function() {
     $("#play").click(function() {
         $("#bulle").text("Veuillez entrer un nombre positif minimum");
         $("#champ").submit(function() {
-            var min = $("#nom").val();
+            min = $("#nom").val();
             console.log(min);
             $("#play").unbind("click");
         });
         $("#submit").click(function() {
-            var min = $("#nom").val();
+            min = $("#nom").val();
             console.log(min);
             $("#bulle").text("Veuillez entrer un nombre positif maximum");
             $("#champ").unbind("submit");
             $("#champ").submit(function() {
-                var max = $("#nom").val();
+                max = $("#nom").val();
                 console.log(max);
-            })
+
+
+
+
+
+
+
+
+
+                numRand = Math.floor(Math.random() * (max - min + 1) | 0 + min);
+            });
+
+            $("#submit").click(function() {
+                Tentative = $("#nom").val();
+                console.log(Tentative);
+                $("#bulle").text("Combien d'essais voulez vous tenter ?");
+                $("#champ").unbind("submit");
+
+                $("#submit").click(function() {
+                    num = $("#nom").val();
+                    console.log(num);
+                    $("#bulle").text("Essayez de trouver le nombre aléatoire ?");
+                    $("#champ").unbind("submit");
+                    $("#champ").submit(function() {
+                        for (i = 0; i < Tentative - 1; i++) {
+
+                            if (numRand > num) {
+                                var num = prompt("c'est plus");
+                            } else if (numRand < num) {
+                                var num = prompt("c'est moins");
+                            } else if (numRand == num) {
+                                alert("GG!!");
+                                break;
+                            }
+                        }
+
+                    });
+                })
+            });
         });
-    })
+    });
 });
 
 
-//     // var min = prompt("Veuillez entrer un nombre positif minimum");
-//     // var max = prompt("Veuillez entrer un nombre positif maximum");
 
-//     var numRand = Math.floor(Math.random() * (max - min + 1) | 0 + min);
+
+
 
 //     // var Tentative = prompt("Combien d'essais voulez vous tenter ?");
 
